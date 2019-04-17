98b19ed2f47a0d2488a41d6ebf596853e5df9fc8
wildjames <jwild2@sheffield.ac.uk>
2019-04-16 23:19:02 -0400

    Indentation error - caught it early enough to be okay with it!

--- scrape_arxiv.py
+++ scrape_arxiv.py
@@ -171,20 +171,20 @@ if __name__ in "__main__":
         for M in range(1, 13):
             codes.append("{:02d}{:02d}".format(Y, M))
 
-    # Every two years, store the data we have so far
-    if Y % 2 == 0:
-        # Call the arxiv API and get the author lists of each ID in each month.
-        p = Pool(16)
-        records = p.map(scrape_authors, codes)
-        # Graceful finish
-        p.terminate()
-        p.join()
+        # Every two years, store the data we have so far
+        if Y % 2 == 0:
+            # Call the arxiv API and get the author lists of each ID in each month.
+            p = Pool(24)
+            records = p.map(scrape_authors, codes)
+            # Graceful finish
+            p.terminate()
+            p.join()
 
-        with open(ofile, 'a') as f:
-            for entry in records:
-                for key in entry.keys():
-                    authors = [x.replace(',', '').replace('"', '').replace("'") for x in entry[key]]
-                    a = ','.join(authors)
-                    f.write("{}, {}".format(key, a))
+            with open(ofile, 'a') as f:
+                for entry in records:
+                    for key in entry.keys():
+                        authors = [x.replace(',', '').replace('"', '').replace("'") for x in entry[key]]
+                        a = ','.join(authors)
+                        f.write("{}, {}".format(key, a))
     
     os.remove('proxies.tmp')
