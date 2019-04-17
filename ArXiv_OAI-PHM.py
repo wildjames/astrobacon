e8b474069eceb49e70a31a471ea13dbfc46b460c
wildjames <jwild2@sheffield.ac.uk>
2019-04-17 19:00:20 -0400

    More illuminating error handling

--- ArXiv_OAI-PHM.py
+++ ArXiv_OAI-PHM.py
@@ -11,30 +11,36 @@ with open('ArXiv_OAI.txt', 'w') as f:
     for record in records:
         try:
             paper = record.metadata['id'][0]
-
-            forenames = record.metadata['forenames']
-            surnames = record.metadata['keyname']
-            authors = []
-            for firstName, lastName in zip(forenames, surnames):
-                a = "{} {}".format(firstName, lastName)
-                authors.append(a)
-
-            authors = ', '.join(authors)
-            line = "{}, {}\n".format(paper, authors)
-            f.write(line)
         except:
-            paper = record.metadata['id']
-            if type(paper) is list:
-                paper = paper[0]
-            else:
-                print("Something weird!")
-                pprint(record)
-            a = ', '.join(record.metadata['keyname'])
-            line = "{}, {}\n".format(paper, a)
-            f.write(line)
-        else:
-            print("\n\n\n\nThe following paper was problematic - it probably has a collaboration or something similar.")
+            print("Couldn't get the paper ID!")
             pprint(record.metadata)
-            print(record.metadata['id'])
-            print(record.metadata['keyname'])
-            print(record.metadata['forenames'])
+
+            continue
+
+        try:
+            surnames = record.metadata['keyname']
+        except:
+            print("Couldn't get the paper keynames!")
+            pprint(record.metadata)
+
+            continue
+
+        try:
+            forenames = record.metadata['forenames']
+        except:
+            print("Couldn't get the paper Forenames!")
+            pprint(record.metadata)
+            forenames = ['' for _ in surnames]
+
+
+        authors = []
+        for firstName, lastName in zip(forenames, surnames):
+            a = "{} {}".format(firstName, lastName)
+            authors.append(a)
+
+        authors = ', '.join(authors)
+        line = "{}, {}\n".format(paper, authors)
+        
+        print(line, end='')
+        f.write(line)
+        
